272909ccdee49e317468c2077797b709caf52f38
Phu T. Van <pvan@fredhutch.org>
2019-12-18 10:41:09 -0800

    added #20 factorial digit sum

--- euler020.py
+++ euler020.py
@@ -1,8 +1,8 @@
 # Project Euler problem 020
 # Find the sum of the digits in the number 100!
 
-# this is trivial with built-in math functions and list comprehension
 # unlike R, Python readily handles large integers
+# so this is trivial with built-in math functions and list comprehension
 
 import math
 
