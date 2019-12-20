a2405cda67e829f9c143aff508350c5aaac5cb32
Phu T. Van <pvan@fredhutch.org>
2019-12-20 10:36:13 -0800

    added #24 lexicographic permutations

--- euler024.py
+++ euler024.py
@@ -6,8 +6,30 @@
 # What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
 # brute force version using itertools
+import numpy as np
 from itertools import permutations
 
 digit = ''.join(list(permutations('0123456789', 10))[999999])
 print(digit)
 
+
+# smarter version ported from euler024.R
+
+def lex(seq, n):
+    seq = np.array(seq)
+    n = int(n)
+    length = len(seq)
+    vec = np.zeros(length, dtype="int")
+    for i in range(0, length-1):
+        if n == 0:
+            vec[i] = seq[len(seq)-1]
+        else:
+            tmp = np.prod(range(1, length - i))
+            div = int((n-1) / tmp)
+            vec[i] = seq[div]
+            n = n % tmp
+            seq = seq[seq != vec[i]]
+
+    return vec
+
+lex(range(0,10),1e6)
